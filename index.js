cc1714cdda319b09f1ed746a4a2d3cf8f3956e54
sourabhsharma1992 <164200389+sourabhsharma1992@users.noreply.github.com>
2024-03-20 20:08:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('Hello World All Guys!')
+  res.send('Hello World All Guyssss!')
 })
 
 app.get('/status', (req, res) => {
